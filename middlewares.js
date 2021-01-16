37fa10536b16ec443e9a458416c1330e43dc51c8
Park Taehyeong <2468ab@naver.com>
2021-01-16 16:25:53 +0900

    2021-01-16

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,12 @@
+import { videos } from "./db";
 import routes from "./routes";
 
 export const localMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated: true,
+        id: 1
+    };
     next();
 };
